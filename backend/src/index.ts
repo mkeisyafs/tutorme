e313964e0dbcd0fda4ff923f065c57f2dda70955
mkeisyafs <mobilezero24@gmail.com>
2026-07-20 20:11:26 +0700

    add PORT to .env

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -49,8 +49,8 @@ const app = new Elysia({ prefix: "/api" })
   .use(questionRoute)
   .use(examSubmissionRoute)
 
-  .listen(5000);
+  .listen(process.env.PORT || 5000);
 
 console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+  `🦊 Elysia is running at http://${app.server?.hostname}:${app.server?.port}`
 );
